ca4239c4b362efbb83d0ce0380f3073bfcd7147a
antonio <antonio@antonios-mbp.home>
2020-07-12 06:01:10 -0400

    implemented tensor flow for both UIView and able to predict Handwriting answer of user

--- Multiplication App/PracticeMutlViewController/MultiplicationViewController.swift
+++ Multiplication App/PracticeMutlViewController/MultiplicationViewController.swift
@@ -9,69 +9,252 @@
 import UIKit
 import VisionKit
 import Vision
+import Sketch
 
 class MultiplicationViewController: UIViewController {
     
     
     var textRecognitionRequest = VNRecognizeTextRequest()
-    var reconginzedText = ""
+    
+    var textRecognitionRequestSecond = VNRecognizeTextRequest()
+
+    
+    var reconginzedText = "" {
+        didSet {
+            enterButton.setTitle("Enter answer as \(reconginzedTextSecond)\(reconginzedText)", for: .normal)
+        }
+    }
+    
+    var reconginzedTextSecond = "" {
+        didSet {
+        enterButton.setTitle("Enter answer as \(reconginzedTextSecond)\(reconginzedText)", for: .normal)
+        }
+    }
+
     
     //    var drawingToTextImage: UIImage!
     var SampleImage = UIImage.init(named: "maxresdefault")!
     
+    @IBOutlet weak var enterButton: UIButton!
     
-    @IBOutlet weak var CanvasView: CanvasView!
+    @IBOutlet weak var sketchView: SketchView!
+    
+    @IBOutlet weak var sketchView2: SketchView!
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var bottomlabel: UILabel!
+    
+    
+    private var classifier: DigitClassifier?
+    
+    private var secondClassifier: DigitClassifier?
+    
+    var lastPoint = CGPoint.zero
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        textRecognitionRequest = VNRecognizeTextRequest(completionHandler: { (request, error) in
-            if let results = request.results, !results.isEmpty {
-                if let requestResults = request.results as? [VNRecognizedTextObservation] {
-                    self.reconginzedText = ""
-                    for observation in requestResults {
-                        guard let candidate = observation.topCandidates(1).first else { return }
-                        self.reconginzedText += candidate.string
-                        self.reconginzedText += "\n"
-                    }
-                    print(self.reconginzedText)
-                    print("here")
-                }
+
+        // Setup sketch view.
+        sketchView.lineWidth = 10//30
+        sketchView.backgroundColor = UIColor.black
+        sketchView.lineColor = UIColor.white
+        sketchView.sketchViewDelegate = self
+        
+        sketchView2.lineWidth = 10//30
+        sketchView2.backgroundColor = UIColor.black
+        sketchView2.lineColor = UIColor.white
+        sketchView2.sketchViewDelegate = self
+        
+        
+        
+        // Initialize a DigitClassifier instance
+        
+        
+        
+        DigitClassifier.newInstance { result in
+            switch result {
+            case let .success(classifier):
+                self.secondClassifier = classifier
+                self.classifier = classifier
+            case .error(_):
+                print("Failed to initialize.")
+                //            self.resultLabel.text = "Failed to initialize."
             }
-        })
-        textRecognitionRequest.recognitionLevel = .fast
-        textRecognitionRequest.usesLanguageCorrection = true
-//        textRecognitionRequest.
-//        textRecognitionRequest.
-//        textRecognitionRequest.minimumTextHeight = 80
-        textRecognitionRequest.customWords = ["1","2","3","4","5","6","7","8","9","0"]
-//        textRecognitionRequest.
-//        textRecognitionRequest.usesLanguageCorrection = false
+        }
+
     }
     
+    
+
     
     @IBAction func EnterAnswer(_ sender: UIButton) {
-//        SampleImage.cgImage!
-        let myImage = CanvasView.asImage()
-        let handler = VNImageRequestHandler(cgImage: myImage.cgImage!, options: [:])
-        do {
-            try handler.perform([textRecognitionRequest])
-        } catch {
-            print(error)
-        }
+        print("Your answer is")
+        print("\(reconginzedTextSecond)\(reconginzedText)")
+        sketchView.clear()
+        sketchView2.clear()
     }
-    
-    
+
+
+
     @IBAction func clearCanvas(_ sender: UIButton) {
-        CanvasView.clearCanvas()
+
+        if sender.isSelected {
+            print("True")
+            sketchView.drawTool = .pen
+            sketchView2.drawTool = .pen
+            sketchView.lineWidth = 10
+            sketchView2.lineWidth = 10
+            sender.isSelected = false
+            sender.setImage(UIImage.init(named: "eraser"), for: .normal)
+        } else {
+            sketchView.drawTool = .eraser
+            sketchView2.drawTool = .eraser
+            sketchView2.lineWidth = 26
+            sketchView.lineWidth = 26
+            print("False")
+            sender.isSelected = true
+            sender.setImage(UIImage.init(named: "pencil"), for: .normal)
+
+        }
+        
     }
     
     func updateDrawing(image:UIImage) {
         let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
         do {
             try handler.perform([textRecognitionRequest])
+
+            try handler.perform([textRecognitionRequestSecond])
+
         } catch {
             print(error)
         }
     }
+}
+
+
+// MARK: Sketch Delegate
+
+extension MultiplicationViewController: SketchViewDelegate {
+    
+    func drawView(_ view: SketchView, didEndDrawUsingTool tool: AnyObject) {
+        if view == sketchView2 {
+            print("shittt")
+            
+            func updateDrawing(image:UIImage) {
+                let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
+                do {
+
+                    try handler.perform([textRecognitionRequestSecond])
+
+                } catch {
+                    print(error)
+                }
+            }
+
+            classifySecondDrawing()
+
+            
+        } else if view == sketchView {
+            print("Fuckkkkk")
+            
+            
+            func updateDrawing(image:UIImage) {
+                let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
+                do {
+                    try handler.perform([textRecognitionRequest])
+
+                } catch {
+                    print(error)
+                }
+            }
+            
+            classifyFirstDrawing()
+
+        }
+      
+    }
+ 
+    
+    private func classifyFirstDrawing() {
+        
+      guard let classifier = self.classifier else { return }
+      // Capture drawing to RGB file.
+      UIGraphicsBeginImageContext(sketchView.frame.size)
+        
+      sketchView.layer.render(in: UIGraphicsGetCurrentContext()!)
+
+        
+      let drawing = UIGraphicsGetImageFromCurrentImageContext()
+      UIGraphicsEndImageContext();
+
+        
+      guard drawing != nil else {
+        print("Invalid drawing.")
+//        resultLabel.text = "Invalid drawing."
+        return
+      }
+
+        
+      // Run digit classifier.
+      classifier.classify(image: drawing!) { result in
+        // Show the classification result on screen.
+        switch result {
+        case let .success(classificationResult):
+            
+            let rightAnswer = classificationResult.split(separator: "\n").first
+            if let rightAnswer = rightAnswer?.last {
+                self.reconginzedText = rightAnswer.description
+                print(rightAnswer)
+            } else {
+              self.reconginzedText = "Error"
+            }
+            
+            
+        case .error(_):
+            print("Failed to classify drawing.")
+//          self.resultLabel.text = "Failed to classify drawing."
+        }
+      }
+
+    }
+    
+    
+    
+    private func classifySecondDrawing() {
+         guard let secondClassifier = self.secondClassifier else { return }
+          // Capture drawing to RGB file.
+          UIGraphicsBeginImageContext(sketchView2.frame.size)
+          sketchView2.layer.render(in: UIGraphicsGetCurrentContext()!)
+
+            
+            
+          let drawing = UIGraphicsGetImageFromCurrentImageContext()
+          UIGraphicsEndImageContext();
+
+          guard drawing != nil else {
+            print("Invalid drawing.")
+    //        resultLabel.text = "Invalid drawing."
+            return
+          }
+            secondClassifier.classify(image: drawing!) { result in
+                    // Show the classification result on screen.
+                    switch result {
+                    case let .success(classificationResult):
+                        let leftAnswer = classificationResult.split(separator: "\n").first
+                        
+                        if let leftAnswer = leftAnswer?.last {
+                            self.reconginzedTextSecond = leftAnswer.description
+                            print(leftAnswer)
+                        } else {
+                          self.reconginzedTextSecond = "Error i dont know that number"
+                        }
+                    case .error(_):
+                        print("Failed to classify drawing.")
+            //          self.resultLabel.text = "Failed to classify drawing."
+                    }
+                  }
+        }
 }
